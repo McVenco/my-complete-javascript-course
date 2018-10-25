b12026296e4753ca076a212e2eb0e78bf51541be
McVenco <mcvenco@gmail.com>
2018-10-25 21:47:14 +0200

    Section 2.10 code examples, with some extras I added

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -22,7 +22,7 @@
 
     // Variable naming rules: variables cannot start with numbers or special characters, except $ or _
     var _3years = 3;
-    var ericMarina = 'Eric en Marina';
+    var ericMarina = 'Eric and Marina';
 
     // var if = 23;
     // this is not allowed: "if" is a reserved name in Javascript, just like "function", "this", "delete" etc.
@@ -54,7 +54,48 @@
 
     alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
 
-    var lastName = prompt('What is his last Name?');
+    var lastName = prompt('What is his last name?');
     console.log(firstName + ' ' + lastName);
+
+})();
+
+(function section2_10(){
+    /*
+
+    Basic operators
+
+    */
+
+    var year, yearEric, yearMarina;
+    currentYear = 2018;
+    ageEric = 41;
+    ageMarina = 35;
+
+    // Math operators
+    yearEric = currentYear - ageEric;
+    yearMarina = currentYear - ageMarina;
+
+    console.log(yearEric);
+
+    console.log(currentYear + 2);
+    console.log(currentYear * 2);
+    console.log(currentYear / 10);
+
+    // Logical operators
+    var ericOlder = ageEric > ageMarina;
+    console.log(ericOlder);
+
+    // typeof operator
+    console.log(typeof ericOlder);                      // boolean
+    console.log(typeof ageEric);                        // number
+    console.log(typeof 'Eric is older than Marina');    // string
+    
+    var x;
+    console.log(typeof x);                              // undefined
+    
+    var funcTest = function(){};
+    console.log(typeof funcTest);                       // function
+
+    console.log(typeof 1/0);                            // NaN
 
 })();
