39cee35b2ed1da2c2a2d9409233782551db43348
McVenco <mcvenco@gmail.com>
2018-10-25 21:31:02 +0200

    Indent plus minor text changes

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,60 +1,60 @@
 (function section2_8(){
-/*
+    /*
 
-Variables and data types
+    Variables and data types
 
-*/
+    */
 
-var firstName = 'Eric';
-console.log(firstName);
+    var firstName = 'Eric';
+    console.log(firstName);
 
-var lastName = 'Vonk';
-var age = 41;
+    var lastName = 'Vonk';
+    var age = 41;
 
-var fullAge = true;
-console.log(fullAge);
+    var fullAge = true;
+    console.log(fullAge);
 
-var job;
-console.log(job);
+    var job;
+    console.log(job);
 
-job = 'Front end developer';
-console.log(job);
+    job = 'Front end developer';
+    console.log(job);
 
-// Variable naming rules: variables cannot start with numbers or special characters, except $ or _
-var _3years = 3;
-var ericMarina = 'Eric en Marina';
+    // Variable naming rules: variables cannot start with numbers or special characters, except $ or _
+    var _3years = 3;
+    var ericMarina = 'Eric en Marina';
 
-// var if = 23;
-// this is not allowed: "if" is a reserved name in Javascript, just like "function", "this", "delete" etc.
+    // var if = 23;
+    // this is not allowed: "if" is a reserved name in Javascript, just like "function", "this", "delete" etc.
 
 })();
 
 (function section2_9(){
-/*
+    /*
 
-Variable mutation and type coercion
+    Variable mutation and type coercion
 
-*/
+    */
 
-var firstName = 'Eric';
-var age = 41;
+    var firstName = 'Eric';
+    var age = 41;
 
-// Type coercion
-console.log(firstName + ' ' + age);
+    // Type coercion
+    console.log(firstName + ' ' + age);
 
-var job, isMarried;
-job = 'Front end developer';
-isMarried = true;
+    var job, isMarried;
+    job = 'Front end developer';
+    isMarried = true;
 
-console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+    console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
 
-// Variable mutation
-age = 'twenty eight';
-job = 'driver';
+    // Variable mutation
+    age = 'forty one';
+    job = 'writer';
 
-alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+    alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
 
-var lastName = prompt('What is his last Name?');
-console.log(firstName + ' ' + lastName);
+    var lastName = prompt('What is his last Name?');
+    console.log(firstName + ' ' + lastName);
 
 })();
