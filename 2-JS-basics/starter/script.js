773543c2ce8495da0132c1bbae0994ab347bdd3f
McVenco <mcvenco@gmail.com>
2018-10-25 21:03:52 +0200

    Add script.js file and first code examples (from section 2.8)

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,27 @@
+/*
+
+Variables and data types
+
+*/
+
+var firstName = 'Eric';
+console.log(firstName);
+
+var lastName = 'Vonk';
+var age = 41;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Front end developer';
+console.log(job);
+
+// Variable naming rules: variables cannot start with numbers or special characters, except $ or _
+var _3years = 3;
+var ericMarina = 'Eric en Marina';
+
+// var if = 23;
+// this is not allowed: "if" is a reserved name in Javascript, just like "function", "this", "delete" etc.
