b49c3c1e90884015250293b89f1fa3f72d64aa31
McVenco <mcvenco@gmail.com>
2018-10-27 22:34:38 +0200

    Section 2.18 - (I used switch statements rather than if/else statements)

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -319,3 +319,68 @@
     }
 
 })();
+
+(function section2_18(){
+    /*
+
+    Coding challenge #2
+
+
+    John and Mike both play basketball in different teams.
+    In the latest 3 games, John's team scored 89, 120 and 103 points,
+    while Mike's team scored 116, 94 and 123 points.
+
+    1. Calculate the average score for each team
+    2. Decide which teams wins in average (highest average score),
+       and print the winner to the console. Also include the average score in the output.
+    3. Then change the scores to show different winners.
+       Don't forget to take into account there might be a draw (the same average score)
+
+    4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points.
+       Like before, log the average winner to the console.
+       HINT: you will need the && operator to take the decision.
+       If you can't solve this one, just watch the solution, it's no problem :)
+    5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
+
+    */
+
+    var scoreJohn = (89 + 120 + 103) / 3;
+    var scoreMike = (116 + 94 + 123) / 3;
+ 
+    switch(true) {
+        case scoreJohn > scoreMike:
+            console.log('John wins with average score ' + scoreJohn);
+            break;
+        case scoreJohn < scoreMike:
+            console.log('Mike wins with average score ' + scoreMike);
+            break;
+        default:
+            console.log('the score is equal!')
+    }
+
+    var scoreMary = (97 + 134 + 105) / 3;
+
+    switch(true) {
+        case scoreJohn > scoreMike && scoreJohn > scoreMary:
+            console.log('John wins with average score ' + scoreJohn);
+            break;
+        case scoreMike > scoreJohn && scoreMike > scoreMary:
+            console.log('Mike wins with average score ' + scoreMike);
+            break;
+        case scoreMary > scoreJohn && scoreMary > scoreMike:
+            console.log('Mary wins with average score ' + scoreMary);
+            break;
+        case scoreMary === scoreJohn && scoreMary > scoreMike:
+            console.log('Mary and John both win with average score ' + scoreMary);
+            break;
+        case scoreJohn === scoreMike && scoreJohn > scoreMary:
+            console.log('John and Mike both win with average score ' + scoreJohn);
+            break;
+        case scoreMike === scoreMary && scoreMike > scoreJohn:
+            console.log('Mike and Mary both win with average score ' + scoreMike);
+            break;
+        default:
+            console.log('There are three winners with score ' + scoreJohn + '!');
+    }
+
+})();
