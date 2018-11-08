fe3420ec7dd971ae541602cad20fd508fbc237ce
Eric Vonk <mcvenco@gmail.com>
2018-11-08 10:18:29 +0100

    Coding challenge #6, part 1 - my solution (rather complicated with a state variable, but it works)

--- 4-DOM-pig-game/starter/challenges.js
+++ 4-DOM-pig-game/starter/challenges.js
@@ -1,17 +1,17 @@
 /*
-GAME RULES:
+YOUR 3 CHALLENGES
+Change the game to follow these rules:
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
+1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
+2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
+3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
 */
 
 var scores, roundScore, activePlayer, gamePlaying;
 
 init();
+
+var previousIsSix;
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -38,9 +38,22 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         diceDOM.src = 'dice-' + dice + '.png';
         
         // 3. Update the round score IF the rolled number was NOT a 1
-        if (dice !== 1){
+        if (dice !== 1 && dice !== 6){
+            previousIsSix = false;
             roundScore += dice;
-            document.querySelector('#current-' + activePlayer).textContent = roundScore;     
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else if(dice === 6) {
+            if (previousIsSix){
+                console.log('tweede keer 6')
+                scores[activePlayer] = 0;
+                previousIsSix = false;
+                nextPlayer();
+            } else {
+                console.log('eerste keer 6')
+                previousIsSix = true;
+                roundScore += dice;
+                document.querySelector('#current-' + activePlayer).textContent = roundScore;
+            }
         } else {
             nextPlayer();
         }
@@ -71,6 +84,7 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 function nextPlayer(){
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
+    previousIsSix = false;
 
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
@@ -88,6 +102,7 @@ function init(){
     roundScore = 0;
     activePlayer = 0;
     gamePlaying = true;
+    previousIsSix = false;
     
     document.querySelector('.dice').style.display = 'none';
 
@@ -103,13 +118,3 @@ function init(){
     document.querySelector('.player-1-panel').classList.remove('active');
     document.querySelector('.player-0-panel').classList.add('active');
 }
-
-
-/*
-YOUR 3 CHALLENGES
-Change the game to follow these rules:
-
-1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
-2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
-3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
-*/
