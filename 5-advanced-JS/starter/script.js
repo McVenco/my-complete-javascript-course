aef001fea0727b123c8b385216db2d0b504dda42
McVenco <mcvenco@gmail.com>
2018-11-15 22:59:48 +0100

    Section 5.68 - Closures

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -211,19 +211,79 @@
         console.log(score >= 5);
     }
     game();
-    
-    
+
+
     (function () {
         var score = Math.random() * 10;
         console.log(score >= 5);
     })();
-    
+
     //console.log(score);
-    
-    
+
+
     (function (goodLuck) {
         var score = Math.random() * 10;
         console.log(score >= 5 - goodLuck);
     })(5);
+
+})();
+
+(function section5_68(){
+
+    // Closures
+
+    function retirement(retirementAge){
+        var a = ' years left until retirement.';
+        return function(yearOfBirth){
+            var age = 2018 - yearOfBirth;
+            console.log((retirementAge - age) + a);
+        }
+    }
+
+    var retirementUS = retirement(66);
+    var retirementGermany = retirement(65);
+    var retirementIceland = retirement(67);
+    
+    retirementGermany(1990);
+    retirementUS(1990);
+    retirementIceland(1990);
+
+    // retirement(67)(1982);
+
+    // function from section 5.66:
+    /*
+    function interviewQuestion(job) {
+        if (job === 'designer') {
+            return function(name) {
+                console.log(name + ', can you please explain what UX design is?');
+            }
+        } else if (job === 'teacher') {
+            return function(name) {
+                console.log('What subject do you teach, ' + name + '?');
+            }
+        } else {
+            return function(name) {
+                console.log('Hello ' + name + ', what do you do?');
+            }
+        }
+    }
+
+    */
+
+    // rewritten as closure:
+    function interviewQuestion(job){
+        return function(name){
+            if (job === 'designer') {
+                console.log(name + ', can you please explain what UX design is?');
+            } else if (job === 'teacher') {
+                console.log('What subject do you teach, ' + name + '?');
+            } else {
+                console.log('Hello ' + name + ', what do you do?');
+            }
+        }
+    }
+
+    interviewQuestion('teacher')('John');
+    interviewQuestion('designer')('Mark');
 
 })();
