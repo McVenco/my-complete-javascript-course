bdd18b601bed135b2bbcea5dc371ed05ea971754
McVenco <mcvenco@gmail.com>
2018-11-15 21:01:54 +0100

    Section 5.66 - functions returning functions

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -170,3 +170,34 @@
     console.log(ageHeartRate);
 
 })();
+
+(function section5_66(){
+
+    // First Class Functions: functions returning functions
+
+    function interviewQuestion(job) {
+        if (job === 'designer') {
+            return function(name) {
+                console.log(name + ', can you please explain what UX design is?');
+            }
+        } else if (job === 'teacher') {
+            return function(name) {
+                console.log('What subject do you teach, ' + name + '?');
+            }
+        } else {
+            return function(name) {
+                console.log('Hello ' + name + ', what do you do?');
+            }
+        }
+    }
+
+    var teacherQuestion = interviewQuestion('teacher');
+    var designerQuestion = interviewQuestion('designer');
+
+    teacherQuestion('John');
+    designerQuestion('Mark');
+    designerQuestion('Jane');
+
+    interviewQuestion('teacher')('Mark');
+
+})();
