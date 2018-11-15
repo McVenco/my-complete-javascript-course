4e07a6dea4234cb33e8abb44d5f436e7991aac5a
McVenco <mcvenco@gmail.com>
2018-11-15 21:11:42 +0100

    Section 5.67 - IIFE

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -201,3 +201,29 @@
     interviewQuestion('teacher')('Mark');
 
 })();
+
+(function section5_67(){
+
+    // Immediately Invoked Function Expressions (IIFE)
+
+    function game() {
+        var score = Math.random() * 10;
+        console.log(score >= 5);
+    }
+    game();
+    
+    
+    (function () {
+        var score = Math.random() * 10;
+        console.log(score >= 5);
+    })();
+    
+    //console.log(score);
+    
+    
+    (function (goodLuck) {
+        var score = Math.random() * 10;
+        console.log(score >= 5 - goodLuck);
+    })(5);
+
+})();
