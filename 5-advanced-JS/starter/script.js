759632e681e7317388dfbc6eee680fa1d83dd430
McVenco <mcvenco@gmail.com>
2018-11-16 22:24:59 +0100

    Section 5.69 - bind, call and apply

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -287,3 +287,71 @@
     interviewQuestion('designer')('Mark');
 
 })();
+
+(function section5_69(){
+
+    // Bind, call and apply
+
+    var john = {
+        name: 'John',
+        age: 26,
+        job: 'teacher',
+        presentation: function(style, timeOfDay) {
+            if (style === 'formal') {
+                console.log('Good ' + timeOfDay + ', Ladies and gentlemen! I\'m ' +  this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+            } else if (style === 'friendly') {
+                console.log('Hey! What\'s up? I\'m ' +  this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+            }
+        }
+    };
+    
+    var emily = {
+        name: 'Emily',
+        age: 35,
+        job: 'designer'
+    };
+    
+    john.presentation('formal', 'morning');
+    
+    john.presentation.call(emily, 'friendly', 'afternoon');
+    
+    //john.presentation.apply(emily, ['friendly', 'afternoon']);
+    
+    var johnFriendly = john.presentation.bind(john, 'friendly');
+    
+    // this is called currying:
+    // "currying is the process of breaking down a function
+    // into a series of functions that each take a single argument."
+    // https://hackernoon.com/currying-in-js-d9ddc64f162e
+    johnFriendly('morning');
+    johnFriendly('night');
+
+    var emilyFormal = john.presentation.bind(emily, 'formal');
+    emilyFormal('afternoon');
+
+
+    // Another cool example
+    var years = [1990, 1965, 1937, 2005, 1998];
+
+    function arrayCalc(arr, fn) {
+        var arrRes = [];
+        for (var i = 0; i < arr.length; i++) {
+            arrRes.push(fn(arr[i]));
+        }
+        return arrRes;
+    }
+
+    function calculateAge(el) {
+        return 2016 - el;
+    }
+
+    function isFullAge(limit, el) {
+        return el >= limit;
+    }
+
+    var ages = arrayCalc(years, calculateAge);
+    var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+    console.log(ages);
+    console.log(fullJapan);
+    
+})();
